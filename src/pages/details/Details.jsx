872e66441a98d5d34dd99b893372a0c20bdd907f
HP PROBOOK <decorcecolin@gmail.com>
2024-08-19 12:21:16 +0100

    first commit

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,33 @@
+import "./Details.css";
+import { useParams } from "react-router-dom";
+import items from "../../database/food.json"
+import { useEffect, useState } from "react";
+const Details = () => {
+    const {id} = useParams();
+    const [item, setItem] = useState({})
+
+    useEffect(()=>{
+        const theItems = items.filter((e)=> e.id == id );
+        setItem(theItems[0])
+    },[])
+
+  return (
+    <div className="Details">
+        <div className="DetailsHolder">
+            <div className="DetailsImage">
+                <img src={item.image}/>
+            </div>
+            <div className="DetailsDetails">
+                <h3>{item.name}</h3>
+                <p>{item.description}</p>
+                <p>₦ {item.price}</p>
+            </div>
+            <div className="DetailsButton">
+                <button>Add to Cart</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Details
